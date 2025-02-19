55726b1408a4606784d1c98040d467f55f4a0113
kshitij <kshitijtembhurne2001@gmail.com>
2025-02-20 00:50:05 +0530

    read the value form pot if below 40v print low batter & add debounce to emergency button

--- Voice_Alerts_Test.ino
+++ Voice_Alerts_Test.ino
@@ -1,45 +1,67 @@
 const int Emergency_button = 2;
-const int trigPin = 9;  
+const int trigPin = 9;
 const int echoPin = 10;
 int Emergency_button_state = 0;
-int ledPin = 13;
+int lastEmergency_button_state = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+int pot = A0;
+int PotValue = 0;
 float duration, distance;
 
-
-
-
-void setup(){
+void setup() {
   Serial.begin(115200);
   Serial.println("Serial OK");
+
   pinMode(Emergency_button, INPUT);
-  pinMode(trigPin, OUTPUT);  
+  pinMode(pot, INPUT);
+  pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
-void loop(){
-  
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-  digitalWrite(trigPin, LOW);  
-  delayMicroseconds(2);  
-  digitalWrite(trigPin, HIGH);  
-  delayMicroseconds(10);  
-  digitalWrite(trigPin, LOW); 
-  
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
-  
-  Emergency_button_state = digitalRead(Emergency_button);
+  distance = (duration * 0.0343) / 2;
+
+  int reading = digitalRead(Emergency_button);
+
+  if (reading != lastEmergency_button_state) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != Emergency_button_state) {
+      Emergency_button_state = reading;
+
+      if (Emergency_button_state == HIGH) {
+        Serial.println("Emergency_Pressed");
+      }
+    }
+  }
+
+  lastEmergency_button_state = reading;
+
+  PotValue = analogRead(pot);
 
   if (Emergency_button_state == HIGH) {
-    digitalWrite(ledPin, HIGH);
     Serial.println("Emergency_Pressed");
     delay(1000);
-    
-  }else if(distance < 15){
+  }
+  else if (distance < 15) {
     Serial.println("Obstacle_Detected");
-  }else {
-    digitalWrite(ledPin, LOW);
-    Serial.println("Emergency not pressed, Obstacle not detected");
+  }
+  else if (PotValue < 40) {
+    Serial.println("battery_low");
+  }
+  else {
+    Serial.println("Emergency not pressed, Obstacle not detected, battery not low");
     delay(1000);
   }
 }
